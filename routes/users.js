85d4681aff468cd54102d66ea5e470d892108292
Fernando Rojas <juanrojas.jfrc@gmail.com>
2023-06-27 01:14:00 -0500

    asignando rutas para edicion de un usuario

--- routes/users.js
+++ routes/users.js
@@ -54,6 +54,50 @@ router.get('/', async (req, res) => {
   }
 });
 
+//funcion para agregar un nuevo usuario
+
+
+async function agregarUsuario(correo_usuario, nombre, apellido, contraseña) {
+  const query = 'INSERT INTO usuario (correo_usuario, nombre, apellido, contrasena, id_rol, estado) values(?,?,?,?,2,1)' ;
+  const [result] = await conn.query(query, [correo_usuario, nombre, apellido, contraseña]);
+  return result.insertId;
+}
+
+
+// Ruta para agregar un nuevo usuario
+router.post('/', async (req, res) => {
+  try {
+    const correo_usuario = req.body.correo_usuario; // Suponiendo que el objeto del nuevo usuario se encuentra en el cuerpo de la solicitud (req.body)
+    const nombre = req.body.nombre; // Suponiendo que el objeto del nuevo usuario se encuentra en el cuerpo de la solicitud (req.body)
+    const apellido = req.body.apellido; // Suponiendo que el objeto del nuevo usuario se encuentra en el cuerpo de la solicitud (req.body)
+    const contraseña = req.body.contraseña; // Suponiendo que el objeto del nuevo usuario se encuentra en el cuerpo de la solicitud (req.body)
+    
+    const usuarioId = await agregarUsuario(correo_usuario, nombre, apellido, contraseña);
+    res.status(201).json({ message: 'Usuario agregado exitosamente.' });
+  } catch (error) {
+    console.error('Error al agregar el usuario:', error);
+    res.status(500).json({ error: 'Error al agregar el usuario.' });
+  }
+});
+
+
+// Ruta para editar un usuario por su ID
+router.put('/:id', async (req, res) => {
+  try {
+    const usuarioId = req.params.id;
+    const { correo_usuario, nombre, apellido, contraseña } = req.body;
+
+    const query = 'UPDATE usuario SET correo_usuario = ?, nombre = ?, apellido = ?, contrasena = ? WHERE id_usuario = ?';
+    const values = [correo_usuario, nombre, apellido, contraseña, usuarioId];
+
+    await conn.query(query, values);
+    
+    res.status(200).json({ message: 'Usuario actualizado exitosamente.' });
+  } catch (error) {
+    console.error('Error al editar el usuario:', error);
+    res.status(500).json({ error: 'Error al editar el usuario.' });
+  }
+});
 
 
 
